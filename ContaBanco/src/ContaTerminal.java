59fe339a81d9ce0365a7273d437ae0760a650517
Joseph Isbir <joseph.isbir@gmail.com>
2024-06-25 16:24:18 -0300

    Projeto ContaBanco

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,41 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+
+	public static void main(String[] args) {
+
+		Integer numeroConta;
+		String agencia;
+		String nomeCliente;
+		Double saldo;
+		String mensagem = "";
+		
+		Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+		
+		System.out.println("Por favor, digite o seu Nome completo !");
+		nomeCliente = scanner.nextLine();
+		System.out.println("Por favor, digite o número da Agência !");
+		agencia = scanner.next();
+		System.out.println("Por favor, digite o número da Conta! - (Somente números)");
+		numeroConta = scanner.nextInt();
+		System.out.println("Por favor, digite o valor do seu Saldo!");
+		saldo = Double.parseDouble(scanner.next().replace(",", "."));
+		
+		mensagem = mensagem.concat("Olá ")
+		.concat(nomeCliente)
+		.concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+		.concat(agencia)
+		.concat(", conta ")
+		.concat(numeroConta.toString())
+		.concat(" e seu saldo ")
+		.concat(saldo.toString())
+		.concat(" já está disponível para saque!");
+		
+		
+		System.out.println(mensagem);
+		
+		
+	}
+
+}
